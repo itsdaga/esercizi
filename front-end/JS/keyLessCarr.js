a25784184f0cddba1b9d516e31ddf8632f32c0b6
Daga <dagabross@gmail.com>
2022-09-20 20:08:34 +0200

    zozzo

---
+++ front-end/JS/keyLessCarr.js
@@ -0,0 +1,17 @@
+//keyless car
+
+var age = Number(prompt('How old are you?'));
+
+
+
+    if (age < 18){
+
+        alert('PLEASE STOP, I CAN\' FUCK YOU, YOU ARE UNDERAGE');
+    }else if (age === 18){  
+        alert('1ST YEAR READY FOR THE DICK!');
+    }else if (age > 18){
+    
+        alert('wanna smash?');
+    }
+    
+
